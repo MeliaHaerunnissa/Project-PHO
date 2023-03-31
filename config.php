d47185a6eee42cc32db4fc8725665527e5ab898d
Melia Haerunnissa <44825144+MeliaHaerunnissa@users.noreply.github.com>
2023-03-31 11:20:24 +0700

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $koneksi = mysqli_connect("localhost", "root", "", "project_php");
-    if(mysqli_connect_errno())  {
-        echo "Koneksi database  gagal :" . mysqli_connect_errno();
-    }
-?>
